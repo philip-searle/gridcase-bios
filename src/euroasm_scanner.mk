187db82f8cd2cd721e66419ee8ef45d98a4bc9b9
Philip Searle <philip@philip-searle.me.uk>
2024-09-22 22:20:55 +0100

    Build improvements
    Use makepp's custom scanner feature to avoid rebuilding all object files when
    a .inc file changes.  Now, only the dependent .asm files will trigger a
    rebuild.

---
+++ src/euroasm_scanner.mk
@@ -0,0 +1,52 @@
+perl_begin
+{
+	use strict;
+	package Mpp::Scanner::Euroasm;
+	use Mpp::Scanner::C;
+	our @ISA = qw/Mpp::Scanner::C/;
+	sub get_directive {
+		s/^\s*(include)\s*//i ? $1 : undef;
+	}
+	sub factory {
+		Mpp::Scanner::Euroasm->new(@_);
+	}
+	1;
+
+	package Mpp::CommandParser::Euroasm;
+	use Mpp::CommandParser;
+	our @ISA = qw/Mpp::CommandParser/;
+	use Mpp::File;
+	sub factory {
+		shift;
+		Mpp::CommandParser::Euroasm->new(@_);
+	}
+	sub new {
+		my $self = &Mpp::CommandParser::new;
+		$self->{SCANNER} = new Mpp::Scanner::Euroasm($self->rule, $self->dir);
+		$self;
+	}
+	sub xparse_command {
+		my ($self, $command, $setenv) = @_;
+		my @files;
+		my ($binary, @words) = @$command;
+		local $_;
+		while (defined($_ = shift @words)) {
+			push @files, $_;
+		}
+
+		my $scanner = $self->{SCANNER};
+		my $context = $scanner->get_context;
+		$scanner->{LANGUAGE} = 'euroasm';
+		$scanner->add_include_dir(user => '.');
+		$scanner->reset($context);
+		foreach my $file (@files) {
+			$scanner->reset($context);
+			my $xxx = $scanner->scan_file($self, 'user', $file) or return undef;
+		}
+		return 1;
+	}
+	1;
+}
+perl_end
+
+register-parser euroasm.exe Mpp::CommandParser::Euroasm
