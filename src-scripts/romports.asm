255a110e0a9689a2fa34ade8491510087c588e6b
Philip Searle <philip@philip-searle.me.uk>
2024-09-22 22:42:47 +0100

    Update with latets romports code

--- src-scripts/romports.asm
+++ src-scripts/romports.asm
@@ -1,10 +1,5 @@
-ROMPORTS	PROGRAM	OutFile=build/romports.exe, FORMAT=MZ, ENTRY=Init
-
-[CODE]		SEGMENT	purpose=CODE,width=16,align=BYTE,combine=PUBLIC,class=""
-[DATA]		SEGMENT	purpose=DATA,width=16,align=BYTE,combine=PUBLIC,class=""
-[BSS]		SEGMENT	purpose=BSS,width=16,align=BYTE,combine=PUBLIC,class=""
+ROMPORTS	PROGRAM	OutFile=build/romports.exe, FORMAT=COM
 		EUROASM	CPU=286, PRIV=ENABLED, AUTOSEGMENT=ON
-
 
 ; Macro to suppress unused symbol warning for fall-through
 Fallthrough	%macro
@@ -13,32 +8,7 @@ Fallthrough_%.:	equ	%1
 		EUROASM	POP
 		%endmacro
 
-Ports		d w 06F8h, 0000h
-		d w 07FAh, 0000h
-		d w 07F8h, 0000h
-		d w 07F9h, 0000h
-		d w 0BD0h, 0000h
-		d w 0BD1h, 0000h
-PortCount	equ	($ - Ports) / 4
-		; DumpSeg+DumpOffset must be immediately after Ports
-		; because we abuse PortCount+1 to display them without
-		; them being adjustable by the input routines.
-		; Order is also important because we load it with LDS.
-DumpOffset	d w 00000h
-DumpSeg		d w 0A000h
-
-CopyRom		d (16 * 16) * b
-
-%if OFFSET#Ports <> 0
-%error "mov si,Ports" -> "xor si,si" depends on Ports being at offset 0
-%endif
-
 Init		PROC
-		; Init
-		push	PARA#[DATA]
-		pop	ds
-		push	ds
-		pop	es
 
 MainLoop	; Play with ports, grab ROM content
 		mov	cx, PortCount
@@ -191,5 +161,21 @@ ConHex1		PROC
 ; ===========================================================
 ; ===========================================================
 
-;%Display segments
+Ports		d w 06F8h, 0000h
+		d w 07FAh, 0000h
+		d w 07F8h, 0000h
+		d w 07F9h, 0000h
+		d w 0BD0h, 0000h
+		d w 0BD1h, 0000h
+PortCount	equ	($ - Ports) / 4
+		; DumpSeg+DumpOffset must be immediately after Ports
+		; because we abuse PortCount+1 to display them without
+		; them being adjustable by the input routines.
+		; Order is also important because we load it with LDS.
+DumpOffset	d w 00000h
+DumpSeg		d w 0A000h
+
+CopyRom		;d (16 * 16) * b
+
+%Display segments
 		ENDPROGRAM	ROMPORTS
